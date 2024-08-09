6b914505ee7f01a3a919df882fd6d57b2aa438dd
neaguolt <neaguolt@gmail.com>
2024-08-09 10:47:12 +0300

    Add ex01 ft_recursive_factorial

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deb <neaguolt@gmail.com>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 09:53:16 by deb               #+#    #+#             */
+/*   Updated: 2024/08/09 10:46:34 by deb              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb);
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb == 0)
+		return (1);
+	if (nb < 0)
+		return (0);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
