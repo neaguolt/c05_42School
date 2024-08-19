566b267ec0a1d89a061e80d1fbe7dfe3700ce44e
neaguolt <neaguolt@gmail.com>
2024-08-19 11:43:05 +0300

    Add ex03 ft_recursive_power

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deb <neaguolt@gmail.com>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 18:43:26 by deb               #+#    #+#             */
+/*   Updated: 2024/08/18 18:49:59 by deb              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power);
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
